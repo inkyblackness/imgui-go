87cf10026b529dae5d879750454b83d1507c74a2
the-goodies <gediminas.ziauberis@gmail.com>
2019-05-18 23:48:38 +0300

    fixed enumeration comments i guess

--- MouseCursorFlags.go
+++ MouseCursorFlags.go
@@ -1,23 +1,23 @@
 package imgui
 
+// Enumeration for MouseCursor()
+// User code may request binding to display given cursor by calling SetMouseCursor(), which is why we have some cursors that are marked unused here
 const (
-	// Enumeration for MouseCursor()
-	// User code may request binding to display given cursor by calling SetMouseCursor(), which is why we have some cursors that are marked unused here
 	MouseCursorNone = iota - 1
 	MouseCursorArrow
-	// When hovering over InputText, etc.
+	// MouseCursorTextInput when hovering over InputText, etc.
 	MouseCursorTextInput
-	// (Unused by imgui functions)
+	// MouseCursorResizeAll (Unused by imgui functions)
 	MouseCursorResizeAll
-	// When hovering over an horizontal border
+	// MouseCursorResizeNS when hovering over an horizontal border
 	MouseCursorResizeNS
-	// When hovering over a vertical border or a column
+	// MouseCursorResizeEW when hovering over a vertical border or a column
 	MouseCursorResizeEW
-	// When hovering over the bottom-left corner of a window
+	// MouseCursorResizeNESW when hovering over the bottom-left corner of a window
 	MouseCursorResizeNESW
-	// When hovering over the bottom-right corner of a window
+	// MouseCursorResizeNWSE when hovering over the bottom-right corner of a window
 	MouseCursorResizeNWSE
-	// (Unused by imgui functions. Use for e.g. hyperlinks)
+	// MouseCursorHand (Unused by imgui functions. Use for e.g. hyperlinks)
 	MouseCursorHand
 	MouseCursorCount
 )
