c4e4bb882dc719fe05368e09cb84a9d2d5551d64
the-goodies <gediminas.ziauberis@gmail.com>
2019-05-18 23:26:05 +0300

    Move comments above constants

--- ConfigFlags.go
+++ ConfigFlags.go
@@ -15,6 +15,8 @@ const (
 	ConfigFlagNoMouseCursorChange
 
 	// User storage (to allow your back-end/engine to communicate to code that may be shared between multiple projects. Those flags are not used by core ImGui)
-	ConfigFlagIsSRGB        = 1 << 20 // Application is SRGB-aware.
-	ConfigFlagIsTouchScreen = 1 << 21 // Application is using a touch screen instead of a mouse.
+	// Application is SRGB-aware.
+	ConfigFlagIsSRGB = 1 << 20
+	// Application is using a touch screen instead of a mouse.
+	ConfigFlagIsTouchScreen = 1 << 21
 )
--- MouseCursorFlags.go
+++ MouseCursorFlags.go
@@ -5,12 +5,19 @@ const (
 	// User code may request binding to display given cursor by calling SetMouseCursor(), which is why we have some cursors that are marked unused here
 	MouseCursorNone = iota - 1
 	MouseCursorArrow
-	MouseCursorTextInput  // When hovering over InputText, etc.
-	MouseCursorResizeAll  // (Unused by imgui functions)
-	MouseCursorResizeNS   // When hovering over an horizontal border
-	MouseCursorResizeEW   // When hovering over a vertical border or a column
-	MouseCursorResizeNESW // When hovering over the bottom-left corner of a window
-	MouseCursorResizeNWSE // When hovering over the bottom-right corner of a window
-	MouseCursorHand       // (Unused by imgui functions. Use for e.g. hyperlinks)
+	// When hovering over InputText, etc.
+	MouseCursorTextInput
+	// (Unused by imgui functions)
+	MouseCursorResizeAll
+	// When hovering over an horizontal border
+	MouseCursorResizeNS
+	// When hovering over a vertical border or a column
+	MouseCursorResizeEW
+	// When hovering over the bottom-left corner of a window
+	MouseCursorResizeNESW
+	// When hovering over the bottom-right corner of a window
+	MouseCursorResizeNWSE
+	// (Unused by imgui functions. Use for e.g. hyperlinks)
+	MouseCursorHand
 	MouseCursorCount
 )
