bb73a8c07ddffda34157013398655aa109c22cfd
Christian Haas <christian.haas@sevensuns.at>
2019-12-25 15:24:22 +0100

    extracted Key constants

--- imgui.go
+++ imgui.go
@@ -7,33 +7,6 @@ import (
 	"math"
 )
 
-// User fill ImGuiIO.KeyMap[] array with indices into the ImGuiIO.KeysDown[512] array
-const (
-	KeyTab         = 0
-	KeyLeftArrow   = 1
-	KeyRightArrow  = 2
-	KeyUpArrow     = 3
-	KeyDownArrow   = 4
-	KeyPageUp      = 5
-	KeyPageDown    = 6
-	KeyHome        = 7
-	KeyEnd         = 8
-	KeyInsert      = 9
-	KeyDelete      = 10
-	KeyBackspace   = 11
-	KeySpace       = 12
-	KeyEnter       = 13
-	KeyEscape      = 14
-	KeyKeyPadEnter = 15
-	KeyA           = 16 // for text edit CTRL+A: select all
-	KeyC           = 17 // for text edit CTRL+C: copy
-	KeyV           = 18 // for text edit CTRL+V: paste
-	KeyX           = 19 // for text edit CTRL+X: cut
-	KeyY           = 20 // for text edit CTRL+Y: redo
-	KeyZ           = 21 // for text edit CTRL+Z: undo
-	KeyCOUNT       = 22
-)
-
 // Version returns a version string e.g. "1.23".
 func Version() string {
 	return C.GoString(C.iggGetVersion())
