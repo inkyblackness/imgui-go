d8068e133ff6b5c518a46d2698899201923be4c7
JetSetIlly <stephen.t.illingworth@gmail.com>
2020-02-19 08:45:33 +0000

    added Style.FramePadding()
    added IsItemClicked(), FrameHeight*() and InvisibleButton*()
    
    added DrawList(), AddCircle*() and AddRect*()

--- imguiWrapperTypes.h
+++ imguiWrapperTypes.h
@@ -19,6 +19,7 @@ typedef void *IggGlyphRanges;
 typedef void *IggGuiStyle;
 typedef void *IggInputTextCallbackData;
 typedef void *IggIO;
+typedef unsigned int ImU32;  // 32-bit unsigned integer (often used to store packed colors)
 
 typedef struct tagIggVec2
 {
