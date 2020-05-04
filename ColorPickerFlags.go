e2f7abecca1af53d8bfe8197e10084c7bb8270e1
neclepsio <neclepsio@gmail.com>
2020-05-04 12:51:24 +0200

    Update ColorPickerFlags.go

--- ColorPickerFlags.go
+++ ColorPickerFlags.go
@@ -2,11 +2,11 @@ package imgui
 
 const (
 	// ColorPickerFlagsNone default = 0
-	ColorPickerFlagsNone           = 0
+	ColorPickerFlagsNone = 0
 	// ColorPickerFlagsNoPicker disables picker when clicking on colored square.
-	ColorPickerFlagsNoPicker        = 1 << 2  
+	ColorPickerFlagsNoPicker = 1 << 2
 	// ColorPickerFlagsNoOptions disables toggling options menu when right-clicking on inputs/small preview.
-	ColorPickerFlagsNoOptions       = 1 << 3   
+	ColorPickerFlagsNoOptions = 1 << 3
 	// ColorPickerFlagsNoAlpha ignoreс Alpha component (read 3 components from the input pointer).
 	ColorPickerFlagsNoAlpha = 1 << 1
 	// ColorPickerFlagsNoSmallPreview disables colored square preview next to the inputs. (e.g. to show only the inputs)
@@ -23,27 +23,27 @@ const (
 	// User Options (right-click on widget to change some of them). You can set application defaults using SetColorEditOptions(). The idea is that you probably don't want to override them in most of your calls, let the user choose and/or call SetColorPickerOptions() during startup.
 
 	// ColorPickerFlagsAlphaBar shows vertical alpha bar/gradient in picker.
-	ColorPickerFlagsAlphaBar         = 1 << 16
+	ColorPickerFlagsAlphaBar = 1 << 16
 	// ColorPickerFlagsAlphaPreview displays preview as a transparent color over a checkerboard, instead of opaque.
-	ColorPickerFlagsAlphaPreview     = 1 << 17
+	ColorPickerFlagsAlphaPreview = 1 << 17
 	// ColorPickerFlagsAlphaPreviewHalf displays half opaque / half checkerboard, instead of opaque.
 	ColorPickerFlagsAlphaPreviewHalf = 1 << 18
 	// ColorPickerFlagsRGB sets the format as RGB
-	ColorPickerFlagsRGB              = 1 << 20
+	ColorPickerFlagsRGB = 1 << 20
 	// ColorPickerFlagsHSV sets the format as HSV
-	ColorPickerFlagsHSV              = 1 << 21
+	ColorPickerFlagsHSV = 1 << 21
 	// ColorPickerFlagsHEX sets the format as HEX
-	ColorPickerFlagsHEX              = 1 << 22
+	ColorPickerFlagsHEX = 1 << 22
 	// ColorPickerFlagsUint8 _display_ values formatted as 0..255.
-	ColorPickerFlagsUint8            = 1 << 23
+	ColorPickerFlagsUint8 = 1 << 23
 	// ColorPickerFlagsFloat _display_ values formatted as 0.0f..1.0f floats instead of 0..255 integers. No round-trip of value via integers.
-	ColorPickerFlagsFloat            = 1 << 24
+	ColorPickerFlagsFloat = 1 << 24
 	// ColorPickerFlagsPickerHueBar bar for Hue, rectangle for Sat/Value.
-	ColorPickerFlagsPickerHueBar     = 1 << 25
+	ColorPickerFlagsPickerHueBar = 1 << 25
 	// ColorPickerFlagsPickerHueWheel wheel for Hue, triangle for Sat/Value.
-	ColorPickerFlagsPickerHueWheel   = 1 << 26
+	ColorPickerFlagsPickerHueWheel = 1 << 26
 	// ColorPickerFlagsInputRGB enables input and output data in RGB format.
-	ColorPickerFlagsInputRGB        = 1 << 27,  
+	ColorPickerFlagsInputRGB = 1 << 27
 	// ColorPickerFlagsInputHSV enables input and output data in HSV format.
-	ColorPickerFlagsInputHSV        = 1 << 28,  
+	ColorPickerFlagsInputHSV = 1 << 28
 )
