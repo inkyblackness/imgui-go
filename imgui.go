12bc13cbd1187796590b3463d4ca5e77f7ca2aed
Christian Haas <christian.haas@sevensuns.at>
2020-05-17 09:24:07 +0200

    deprecated Get* functions

--- imgui.go
+++ imgui.go
@@ -1357,25 +1357,47 @@ func SetTabItemClosed(tabOrDockedWindowLabel string) {
 	C.iggSetTabItemClosed(labelArg)
 }
 
-// GetScrollX returns the horizontal scrolling amount [0..GetScrollMaxX()].
+// GetScrollX is deprecated and will be removed in v3.0.0 .
+// Deprecated: Use ScrollX() instead.
 func GetScrollX() float32 {
+	return ScrollX()
+}
+
+// ScrollX returns the horizontal scrolling amount [0..GetScrollMaxX()].
+func ScrollX() float32 {
 	return float32(C.iggGetScrollX())
 }
 
-// GetScrollY returns the vertical scrolling amount [0..GetScrollMaxY()].
+// GetScrollY is deprecated and will be removed in v3.0.0 .
+// Deprecated: Use ScrollY() instead.
 func GetScrollY() float32 {
+	return ScrollY()
+}
+
+// ScrollY returns the vertical scrolling amount [0..GetScrollMaxY()].
+func ScrollY() float32 {
 	return float32(C.iggGetScrollY())
 }
 
-// GetScrollMaxX returns the maximum horizontal scrolling amount: ContentSize.X
-// - WindowSize.X .
+// GetScrollMaxX is deprecated and will be removed in v3.0.0 .
+// Deprecated: Use ScrollMaxX() instead.
 func GetScrollMaxX() float32 {
+	return ScrollMaxX()
+}
+
+// ScrollMaxX returns the maximum horizontal scrolling amount: ContentSize.X - WindowSize.X .
+func ScrollMaxX() float32 {
 	return float32(C.iggGetScrollMaxX())
 }
 
-// GetScrollMaxY returns the maximum vertical scrolling amount: ContentSize.Y -
-// WindowSize.Y .
+// GetScrollMaxY is deprecated and will be removed in v3.0.0 .
+// Deprecated: Use ScrollMaxY() instead.
 func GetScrollMaxY() float32 {
+	return ScrollMaxY()
+}
+
+// ScrollMaxY returns the maximum vertical scrolling amount: ContentSize.Y - WindowSize.Y .
+func ScrollMaxY() float32 {
 	return float32(C.iggGetScrollMaxY())
 }
 
