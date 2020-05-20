d95b7f2bb98a21a475ed60cacccfcd2e45a81954
Christian Haas <christian.haas@sevensuns.at>
2020-05-20 19:35:02 +0200

    Calling Go assert only if condition actually fails in C land
    This not only speeds up execution, this also avoids a potential crash in the examples. Found via #94 .

--- wrapper/ConfigOverride.h
+++ wrapper/ConfigOverride.h
@@ -2,7 +2,7 @@
 
 // Overrides as per standard imconfig.h
 
-extern "C" void iggAssert(int result, char const *expression, char const *file, int line);
-#define IM_ASSERT(_EXPR) iggAssert((_EXPR) != 0, #_EXPR, __FILE__, __LINE__)
+extern "C" void iggAssert(char const *expression, char const *file, int line);
+#define IM_ASSERT(_EXPR) do { if ((_EXPR) == 0) { iggAssert(#_EXPR, __FILE__, __LINE__); } } while(false)
 
 #define IMGUI_DISABLE_OBSOLETE_FUNCTIONS
