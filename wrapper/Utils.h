a85a5a4190e2f6f5ed47c8f219bb1db705c81578
Gediminas <gediminas.ziauberis@gmail.com>
2020-07-14 22:27:45 +0300

    Add ListClipper

--- wrapper/Utils.h
+++ wrapper/Utils.h
@@ -9,6 +9,8 @@ extern "C"
 
 extern void iggSpinner(const char* label, float radius, int thickness);
 extern IggBool iggSplitter(IggBool split_vertically, float thickness, float *size1, float *size2);
+extern void   *iggListClipperInit(int count);
+extern IggBool iggListClipperStep(void *handle, int *display_start, int *display_end);
 
 #ifdef __cplusplus
 }
