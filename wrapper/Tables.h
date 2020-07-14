5a16368fc09281d1911f278bcbb8f37cc01b2cab
Gediminas <gediminas.ziauberis@gmail.com>
2020-07-14 15:06:42 +0300

    fix flags type

--- wrapper/Tables.h
+++ wrapper/Tables.h
@@ -6,9 +6,9 @@
 extern "C" {
 #endif
 
-extern IggBool     iggBeginTable(char const *str_id, int columns_count, ImGuiTableFlags flags, IggVec2 const *outer_size, float inner_width);
+extern IggBool     iggBeginTable(char const *str_id, int columns_count, int flags, IggVec2 const *outer_size, float inner_width);
 extern void        iggEndTable();
-extern void        iggTableNextRow(ImGuiTableRowFlags row_flags, float min_row_height);
+extern void        iggTableNextRow(int row_flags, float min_row_height);
 extern IggBool     iggTableNextCell();
 extern IggBool     iggTableSetColumnIndex(int column_n);
 extern int         iggTableGetColumnIndex();
@@ -16,7 +16,7 @@ extern char const *iggTableGetColumnName(int column_n);
 extern IggBool     iggTableGetColumnIsVisible(int column_n);
 extern IggBool     iggTableGetColumnIsSorted(int column_n);
 extern int         iggTableGetHoveredColumn();
-extern void        iggTableSetupColumn(char const *label, ImGuiTableColumnFlags flags, float init_width_or_weight, unsigned int user_id);
+extern void        iggTableSetupColumn(char const *label, int flags, float init_width_or_weight, unsigned int user_id);
 extern void        iggTableAutoHeaders();
 extern void        iggTableHeader(char const *label);
 // extern const ImGuiTableSortSpecs* iggTableGetSortSpecs();
