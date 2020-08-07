35529299c78565032769ff40b2441b0861d038ab
fourst4r <oxygenmagnet@hotmail.com>
2020-08-07 14:58:01 +1200

    add ImGui::GetTime()

--- wrapper/Main.h
+++ wrapper/Main.h
@@ -7,10 +7,12 @@ extern "C" {
 #endif
 
 extern char const *iggGetVersion(void);
+extern double iggGetTime(void);
 
 extern void iggNewFrame(void);
 extern void iggRender(void);
 extern void iggEndFrame(void);
+
 
 #ifdef __cplusplus
 }
