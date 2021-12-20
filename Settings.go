132c12b153ac1982b85a100059edcfa0ff32049a
neclepsio <neclepsio@gmail.com>
2021-12-20 17:04:53 +0100

    Fix documentation

--- Settings.go
+++ Settings.go
@@ -3,28 +3,28 @@ package imgui
 // #include "wrapper/Settings.h"
 import "C"
 
-// LoadIniSettingsFromDisk loads ini settings from disk
+// LoadIniSettingsFromDisk loads ini settings from disk.
 func LoadIniSettingsFromDisk(fileName string) {
 	fileNameArg, fileNameFin := wrapString(fileName)
 	defer fileNameFin()
 	C.iggLoadIniSettingsFromDisk(fileNameArg)
 }
 
-// LoadIniSettingsFromMemory loads ini settings from memory
+// LoadIniSettingsFromMemory loads ini settings from memory.
 func LoadIniSettingsFromMemory(data string) {
 	dataArg, dataFin := wrapString(data)
 	defer dataFin()
 	C.iggLoadIniSettingsFromMemory(dataArg)
 }
 
-// SaveIniSettingsToDisk saves ini settings to disk
+// SaveIniSettingsToDisk saves ini settings to disk.
 func SaveIniSettingsToDisk(fileName string) {
 	fileNameArg, fileNameFin := wrapString(fileName)
 	defer fileNameFin()
 	C.iggSaveIniSettingsToDisk(fileNameArg)
 }
 
-// SaveIniSettingsToMemory saves ini settings to memory
+// SaveIniSettingsToMemory saves ini settings to memory.
 func SaveIniSettingsToMemory() string {
 	return C.GoString(C.iggSaveIniSettingsToMemory())
 }
